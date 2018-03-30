ff1c8f04ee9ba1fd5c5edbcb0bd20ae82ddb9e2c
连海洋 <lianhaiyang@onemt.com.cn>
2018-03-30 08:52:00 +0800

    依赖分类

--- src/getParam.js
+++ src/getParam.js
@@ -8,6 +8,7 @@
  * @date: 2018-03-11
  */
 export default function getParam(name) {
+
 	if(typeof name !== 'string') throw new Error('要获取的参数名必须为字符串')
 	//这里是去除加号，除公司外的项目可能会有影响
 	const url =  decodeURI(window.location.search.replace(/\+/g,'%20'));
@@ -17,4 +18,5 @@ export default function getParam(name) {
 	if (r !== null)
 		return decodeURI(r[2]);
 	return '';
+
 }
